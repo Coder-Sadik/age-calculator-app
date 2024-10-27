d23dcd312f088fef3cf55c6353d7c57002f8fcf7
Sadik Rahman <srahman2702@gmail.com>
2024-10-27 22:47:03 +0600

    initial commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				Purple: "hsl(259, 100%, 65%)",
+				LightRed: "hsl(0, 100%, 67%)",
+				White: "hsl(0, 0%, 100%)",
+				OffWhite: "hsl(0, 0%, 94%)",
+				LightGrey: "hsl(0, 0%, 86%)",
+				SmokeyGrey: "hsl(0, 1%, 44%)",
+				OffBlack: "hsl(0, 0%, 8%)",
+			},
+		},
+		fontFamily: {
+			Poppins: ["Poppins", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
